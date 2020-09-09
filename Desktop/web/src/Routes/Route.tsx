2b746cc232ec8263e12cdc5a0373ce7ec997630a
matheus <mathews_2010@outlook.com>
2020-09-09 14:38:54 -0300

    implementando o context do auth

---
+++ Desktop/web/src/Routes/Route.tsx
@@ -0,0 +1,6 @@
+import React, {useContext} from 'react'
+import {RouteProps as ReactDOMRouteProps, Route as ReactDOMRoute, Redirect} from 'react-router-dom'
+
+import AuthContext from '../Context/auth'
+
+interface RouteProps extends 
